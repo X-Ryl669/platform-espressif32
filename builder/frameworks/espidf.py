adbc02d4b0cc14e6381403c0d32ed2a53308ec2a
X-Ryl669 <boite.pour.spam@gmail.com>
2022-09-13 07:41:42 +0200

    Upgrade the pyparsing version to fit the latest change in the code

--- builder/frameworks/espidf.py
+++ builder/frameworks/espidf.py
@@ -1045,7 +1045,7 @@ def install_python_deps():
         # https://github.com/platformio/platform-espressif32/issues/635
         "cryptography": ">=2.1.4,<35.0.0",
         "future": ">=0.15.2",
-        "pyparsing": ">=2.0.3,<2.4.0",
+        "pyparsing": ">=3.0",
         "kconfiglib": "==13.7.1",
     }
 
